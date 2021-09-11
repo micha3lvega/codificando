1641b73906a55705a1b5857294c307b744bb11b4
Michael Vega <ec3572h@comcel.com.co>
2021-09-10 23:54:39 -0500

    Inicializar proyecto

---
+++ src/main/java/co/com/soft/technology/codificando/CodificandoApplication.java
@@ -0,0 +1,13 @@
+package co.com.soft.technology.codificando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodificandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodificandoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/soft/technology/codificando/CodificandoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.soft.technology.codificando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodificandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
